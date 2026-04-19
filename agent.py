e256614080444c097fefea07a3f5882872e32a41
Developer <developer@example.com>
2026-04-19 18:12:22 +0530

    deployed in hugginface

--- agent.py
+++
@@ -1,3 +0,0 @@
-import random
-def get_action(state):
-    return random.choice([0,1,2,3])
--- grader.py
+++
@@ -1,4 +0,0 @@
-def grader(reached, steps):
-    if not reached:
-        return 0.1
-    return max(0.1, 1 - steps/20)
